4f2d073c0381312042864e82072936e401490718
mgottval <mgottval@localhost.localdomain>
2014-01-06 14:49:37 +0100

    deleted commented code

--- src/main/java/com/issuetracker/model/CustomField.java
+++ src/main/java/com/issuetracker/model/CustomField.java
@@ -62,7 +62,6 @@ public class CustomField implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof CustomField)) {
             return false;
         }
--- src/main/java/com/issuetracker/model/IssueType.java
+++ src/main/java/com/issuetracker/model/IssueType.java
@@ -49,7 +49,6 @@ public class IssueType implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof IssueType)) {
             return false;
         }
