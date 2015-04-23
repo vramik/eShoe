6d2764dfd79476854f39b799967976ca87ece2cf
vramik <vramik@localhost.localdomain>
2015-04-23 16:42:43 +0200

    removed unnecessary logging

--- src/main/java/com/issuetracker/pages/project/ProjectDetail.java
+++ src/main/java/com/issuetracker/pages/project/ProjectDetail.java
@@ -93,7 +93,6 @@ public class ProjectDetail extends PageLayout {
             setResponsePage(AccessDenied.class);
         }   
         permittedActions = securityService.getPermittedActionsForUserAndItem(TypeId.project, projectId);
-        log.info("Project Actions: " + permittedActions);
         
         projectVersionList = project.getVersions();
         projectComponentList = project.getComponents();
